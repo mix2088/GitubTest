f48fdcff2e4114b8b2ad94a5d950e7296dcf94e1
js20028 <mix2088@nate.com>
2023-09-20 11:30:54 +0900

    Initial Commit

---
+++ GitubTest/GitubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitubTestApp.swift
+//  GitubTest
+//
+//  Created by 곽재선 on 2023/09/20.
+//
+
+import SwiftUI
+
+@main
+struct GitubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
